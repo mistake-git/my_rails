e54a5c582f915d0d693f0413132062b4c94bbc32
mistake-git <acbmstk0402@gmail.com>
2019-10-24 15:25:40 +0000

    edit layout

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'users/:id/edit' => 'users#edit'
   post 'users/create' => 'users#create'
   get 'signup' => 'users#new'
+  get '/users/search' => 'users#search'
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show', as: 'users_show'
   post 'login' => 'users#login'
@@ -18,17 +19,17 @@ Rails.application.routes.draw do
   get 'login' => 'users#login_form'
   get 'users/:id/likes' => 'users#likes'
   get 'users/:id/comments' => 'users#comments'
-  get '/users/search' => 'users#search'
+  
   get '/admin' => 'users#admin'
-
   get '/posts/index' => 'posts#index'
+  get '/posts/search' => 'posts#search'
   get '/posts/new' => 'posts#new'
   post '/posts' => 'posts#create', as: 'posts_create'
   get '/posts/:id' => 'posts#show', as: 'posts_show'
   get '/posts/:id/edit' => 'posts#edit'
   patch '/posts/:id/update' => 'posts#update', as: 'posts_update'
   delete '/posts/:id/destroy' => 'posts#destroy'
-  get '/posts/search' => 'posts#search'
+ 
 
   get '/' => 'home#top'
   get 'about' => 'home#about'
